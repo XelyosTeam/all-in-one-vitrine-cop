594ee1fb2d2d977fb5862c30ccf1d23e332c2c7c
Aros <tvdw59161@hotmail.fr>
2021-06-29 21:58:34 +0200

    Update 2.3.1

--- autoload.php
+++ autoload.php
@@ -1,6 +1,6 @@
 <?php
   /* Utilisation de la base de données */
-  include 'src/models.php';
+  include 'entity/models.php';
 
   /* Fonctions générals d'utilisation */
   include 'src/function.php';
